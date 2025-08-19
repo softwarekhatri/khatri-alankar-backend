9688c9ce95eaabad87613291dafcbdaf173a3c3d
Ankit Khatri <ankit.kumar@birdeye.com>
2025-08-19 20:15:42 +0530

    Updated db connection pooling

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,25 @@
-
 import mongoose from "mongoose";
 import { ENV } from "./env";
 
+let isConnected = false;
+
 export async function connectDB() {
+  if (isConnected) {
+    return mongoose.connection;
+  }
+
   const uri = ENV.MONGODB_URI;
   if (!uri) {
     throw new Error("MONGODB_URI is not set");
   }
+
   mongoose.set("strictQuery", true);
+
   await mongoose.connect(uri, {
-    dbName: "alankarv2"
+    dbName: "alankarv2",
+    maxPoolSize: 3, // control number of concurrent sockets
   } as any);
+
+  isConnected = true;
   return mongoose.connection;
 }
